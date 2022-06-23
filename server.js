ab36d5aeeca5f4078f7a66edd6603023d990a96e
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-23 12:09:45 +0100

    sending message git broadcast emit

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const io = require('socket.io')(30001)
+// const io = require('socket.io')(3001)
 
-io.on('connection', socket => {
-  socket.emit('chat-message', 'Hello, you are welcome')
+// io.on('connection', socket => {
+//   socket.emit('chat-message', 'Hello, you are welcome')
+// })
+
+
+const io = require('socket.io')(8000)
+
+io.on('connection', socket=> {
+  socket.on('send-chart-message', message => {
+    socket.broadcast.emit('chart-message', message)
+  })
 })
