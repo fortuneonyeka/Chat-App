8980bb7943ca8d38e072b83f0816aea4f97a8162
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-23 12:33:16 +0100

    added users object and broadcasted connected user

--- server.js
+++ server.js
@@ -4,10 +4,14 @@
 //   socket.emit('chat-message', 'Hello, you are welcome')
 // })
 
-
+const users = {}
 const io = require('socket.io')(8000)
 
 io.on('connection', socket=> {
+  socket.on("new-user", name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
   socket.on('send-chart-message', message => {
     socket.broadcast.emit('chart-message', message)
   })
